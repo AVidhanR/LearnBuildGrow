e742cb8262b0552e4b33ce13e2776ba06718351d
AVidhanR <itsvidhanreddy@gmail.com>
2024-05-21 20:42:08 +0530

    Update go_lang README.md

--- go_lang/02_variables/main.go
+++ go_lang/02_variables/main.go
@@ -3,7 +3,41 @@ package main
 import "fmt"
 
 func main() {
+
+	// string type
 	var username string = "Vidhan";
 	fmt.Println(username)
-	fmt.Printf("Variable type is: %T", username)
+	fmt.Printf("Variable type is: %T \n\n", username)
+
+	// bool type
+	var isLogged bool = true;
+	fmt.Println(isLogged)
+	fmt.Printf("Variable type is: %T \n\n", isLogged)
+    
+	// unsigned 8-bit integer
+	var smallValue uint8 = 255; // >255 cannot be taken you know the reason
+	fmt.Println(smallValue);
+	fmt.Printf("Variable type is: %T \n\n", smallValue)
+
+	/*
+	uint8       the set of all unsigned  8-bit integers (0 to 255)
+	uint16      the set of all unsigned 16-bit integers (0 to 65535)
+	uint32      the set of all unsigned 32-bit integers (0 to 4294967295)
+	uint64      the set of all unsigned 64-bit integers (0 to 18446744073709551615)
+
+	int8        the set of all signed  8-bit integers (-128 to 127)
+	int16       the set of all signed 16-bit integers (-32768 to 32767)
+	int32       the set of all signed 32-bit integers (-2147483648 to 2147483647)
+	int64       the set of all signed 64-bit integers (-9223372036854775808 to 9223372036854775807)
+
+	float32     the set of all IEEE-754 32-bit floating-point numbers
+	float64     the set of all IEEE-754 64-bit floating-point numbers
+
+	complex64   the set of all complex numbers with float32 real and imaginary parts
+	complex128  the set of all complex numbers with float64 real and imaginary parts
+
+	byte        alias for uint8
+	rune        alias for int32
+	*/
+
 }
