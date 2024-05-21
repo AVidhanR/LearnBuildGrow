90b6a63a88527495a359c8786d4429fbdc4efad3
AVidhanR <itsvidhanreddy@gmail.com>
2024-05-21 21:40:55 +0530

    Update main.go - 03, 04

--- go_lang/03_user_input/main.go
+++ go_lang/03_user_input/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	welcome := "Welcome to User Input"
+	welcome := "Welcome to the pizza app\nPlease rate our pizza between 1 and 5"
 	fmt.Println(welcome)
 
 	fmt.Println("Enter the rating: ")
@@ -17,5 +17,5 @@ func main() {
 	// comma ok || err err
 	// the underscore part "stores" the errors
 	input, _ := reader.ReadString('\n') // only single quotes!
-	fmt.Println("Thanks for the rating...", input)
+	fmt.Println("Thanks for the rating, ", input)
 }
---
+++ go_lang/04_conversion/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv" // new package - to convert
+	"strings" // new package - for now used to remove spaces in input o_o
+)
+
+func main() {
+
+	// same code as in 03_user_input/main.go
+	fmt.Println("Welcome to the pizza app");
+	fmt.Println("Please rate our pizza between 1 and 5");
+	reader := bufio.NewReader(os.Stdin);
+	input, _ := reader.ReadString('\n');
+	fmt.Println("Thanks for the rating, ", input)
+
+	// convert string to float64
+	numberRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	// error handling
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		// no errors? display it!
+		fmt.Println(numberRating)
+	}
+
+}
