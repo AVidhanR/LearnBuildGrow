6d4607d542a97024721d4f55bc2b26dbf333d8c8
AVidhanR <itsvidhanreddy@gmail.com>
2024-05-21 21:22:10 +0530

    Update main.go - 03

--- go_lang/03_user_input/main.go
+++ go_lang/03_user_input/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("User Input\n")
+	welcome := "Welcome to User Input"
+	fmt.Println(welcome)
+
+	fmt.Println("Enter the rating: ")
+	reader := bufio.NewReader(os.Stdin)
+
+	// input is read here V_V 
+	// comma ok || err err
+	// the underscore part "stores" the errors
+	input, _ := reader.ReadString('\n') // only single quotes!
+	fmt.Println("Thanks for the rating...", input)
 }
