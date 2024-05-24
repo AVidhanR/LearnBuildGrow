b0b3dcd182a5de57188e45060b0ecef4f26b0751
AVidhanR <itsvidhanreddy@gmail.com>
2024-05-24 20:22:15 +0530

    Update main.go - 19

--- go_lang/19_web_requests/main.go
+++ go_lang/19_web_requests/main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+// this might defer for you guys - make sure replace the link with yours
+const url = "http://127.0.0.1:3000/go_lang/19_web_requests/main.html"
 
 func main() {
 	fmt.Println("Welcome to web_requests in go_lang")
 	fmt.Println()
+	// HTTP - GET in go_lang
+	response, err := http.Get(url)
+	handleError(err)
+
+	// don't forget to close it guys!
+	defer response.Body.Close()
+
+	fmt.Printf("Response is of type: %T\n", response)
+}
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
