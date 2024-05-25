029c31483bae32450afe4ddf188e49886193ad50
AVidhanR <itsvidhanreddy@gmail.com>
2024-05-25 09:48:01 +0530

    Update main.go - 21

--- go_lang/21_web_handling_req/main.go
+++ go_lang/21_web_handling_req/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -12,8 +13,14 @@ func main() {
 	fmt.Println("Welcome to web_requests_handling in go_lang")
 	fmt.Println()
 
-	// call the GET it here
+	// call the GET here
 	// PerformGetRequest()
+
+	// call the POST here
+	// PerformPostJSONRequest()
+
+	// call the PostForm here
+	PerformPostFormRequest()
 
 }
 
@@ -58,6 +65,7 @@ func PerformGetRequest() {
 	fmt.Println("The content is: ", responseStr.String())
 }
 
+// Public
 func PerformPostJSONRequest() {
 	const myUrl string = "http://localhost:3000/post"
 
@@ -72,6 +80,39 @@ func PerformPostJSONRequest() {
 
 	response, err := http.Post(myUrl, "application/json", requestBody)
 	handleError(err)
+	defer response.Body.Close()
+
+	/*
+		ioutil.ReadAll() is deprecated
+		data bytes, err := ioutil.ReadAll(response.Body)
+	*/
+
+	// create a buffer to store the response body
+	buffer := bytes.NewBuffer(nil)
+
+	// copy the response body to the buffer
+	n, err := io.Copy(buffer, response.Body)
+	handleError(err)
+
+	content := buffer.Bytes()
+
+	fmt.Println("data in the web page: ", string(content), "\nnumber of characters: ", n)
+	fmt.Println()
+}
+
+func PerformPostFormRequest() {
+	const myUrl string = "http://localhost:3000/postForm"
+
+	// form data
+	data := url.Values{}
+	data.Add("firstName", "vidhan")
+	data.Add("lastName", "reddy")
+	data.Add("email", "itsvidhanreddy@go.dev")
+
+	// Post Form request => response
+	response, err := http.PostForm(myUrl, data)
+	handleError(err)
+
 	defer response.Body.Close()
 
 	/*
