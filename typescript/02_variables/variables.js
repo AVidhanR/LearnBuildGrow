21d6299eef2ee82cd5036c7b381ff757d25f5bad
AVidhanR <itsvidhanreddy@gmail.com>
2024-06-04 11:39:09 +0530

    Update variables.ts

--- typescript/02_variables/variables.js
+++ typescript/02_variables/variables.js
@@ -1,5 +1,19 @@
-// create a variable
-// syntax:
-//         let varName: type = value;
+"use strict";
+/* create a variable
+  syntax:
+         let varName: type = value;
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+// string
 var greetings = "Hello Vin";
-console.log(greetings);
+// number
+// if it is obvious of using number
+// no need to specify it! - ts can do it for you
+// the way you use the methods
+var myNum = 6.5;
+myNum.toFixed();
+// boolean
+var isLoggedIn = false;
+console.log(isLoggedIn);
+console.log(myNum);
+console.log(greetings.toUpperCase());
--- typescript/02_variables/variables.ts
+++ typescript/02_variables/variables.ts
@@ -1,11 +1,23 @@
-// create a variable
-// syntax:
-//         let varName: type = value;
+/* create a variable
+  syntax:
+         let varName: type = value;
+*/
+
+// string
 let greetings: string = "Hello Vin";
 
-let myNum: number = 6;
-console.log(myNum);
+// number
+// if it is obvious of using number
+// no need to specify it! - ts can do it for you
+// the way you use the methods
+let myNum = 6.5;
+myNum.toFixed();
 
+// boolean
+let isLoggedIn: boolean = false;
+
+console.log(isLoggedIn);
+console.log(myNum);
 console.log(greetings.toUpperCase());
 
 export {};
