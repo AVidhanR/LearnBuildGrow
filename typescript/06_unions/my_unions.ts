a98a4e968f4875a2df170b7dddc0ec6801c96c9c
AVidhanR <itsvidhanreddy@gmail.com>
2024-06-05 13:41:30 +0530

    Update my_unions.ts

--- typescript/06_unions/my_unions.ts
+++ typescript/06_unions/my_unions.ts
@@ -1,1 +1,12 @@
 // create an union in ts
+// union with types number,string and boolean
+let score: number | string | boolean = 33;
+console.log("score as number: ", score);
+
+// valid
+score = "This is Vidhan";
+console.log("score as string", score);
+
+// valid
+score = true;
+console.log("score as boolean: ", score);
