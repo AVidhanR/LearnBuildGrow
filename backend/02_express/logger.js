5c9aafd957007ef79cef0d93abafba162181551c
AVidhanR <itsvidhanreddy@gmail.com>
2024-06-14 21:52:53 +0530

    Create logger.js

---
+++ backend/02_express/logger.js
@@ -0,0 +1,24 @@
+import { createLogger, format, transports } from "winston";
+const { combine, timestamp, json, colorize } = format;
+
+// Custom format for console logging with colors
+const consoleLogFormat = format.combine(
+  format.colorize(),
+  format.printf(({ level, message, timestamp }) => {
+    return `${level}: ${message}`;
+  })
+);
+
+// Create a Winston logger
+const logger = createLogger({
+  level: "info",
+  format: combine(colorize(), timestamp(), json()),
+  transports: [
+    new transports.Console({
+      format: consoleLogFormat,
+    }),
+    new transports.File({ filename: "app.log" }),
+  ],
+});
+
+export default logger;
