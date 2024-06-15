9f3ccc5a2f7a6f52fc79affa0799f54a268f3bf1
AVidhanR <itsvidhanreddy@gmail.com>
2024-06-15 09:15:14 +0530

    added morganFormat and app.use() to use the logger

--- backend/02_express/index.js
+++ backend/02_express/index.js
@@ -13,6 +13,24 @@ app.use(express.json());
 let teaData = [];
 let nextID = 1;
 
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 app.listen(port, () => {
   console.log(`Server running @ ${port}`);
 });
