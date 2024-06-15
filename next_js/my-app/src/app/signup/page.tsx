97badafdc6e29b538d5064f6bed381c5054cfc9c
AVidhanR <itsvidhanreddy@gmail.com>
2024-06-15 11:35:01 +0530

    Update .env file and signup/login pages

--- next_js/my-app/src/app/signup/page.tsx
+++ next_js/my-app/src/app/signup/page.tsx
@@ -1,5 +1,55 @@
+"use client";
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
 
 export default function SignUpPage() {
-  return <div>SignUpPage</div>;
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignUp = async () => {};
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>SignUp</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        type="text"
+        id="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      />
+      <button
+        onClick={onSignUp}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        SignUp
+      </button>
+      <Link href={"/login"}>Login page</Link>
+    </div>
+  );
 }
