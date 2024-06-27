00c1d8564e47339957ae06c2037dc029fd3ce8c3
AVidhanR <itsvidhanreddy@gmail.com>
2024-06-27 06:23:47 +0530

    Update server port configuration to use environment variable

--- backend/03_vidtube/src/index.js
+++ backend/03_vidtube/src/index.js
@@ -1,6 +1,6 @@
 import { app } from "./app";
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
