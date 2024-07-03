5a216018d500d51ec4b56a37e818032d92fc40be
AVidhanR <itsvidhanreddy@gmail.com>
2024-07-03 15:27:05 +0530

    Configure Express app to parse JSON and URL-encoded form data, and serve static files

--- backend/03_vidtube/src/app.js
+++ backend/03_vidtube/src/app.js
@@ -10,4 +10,14 @@ app.use(
   })
 );
 
+// Configure Express app to parse incoming JSON data
+app.use(express.json({ limit: "16kb" }));
+
+// Configure Express app to parse incoming URL-encoded form data
+// with extended option enabled for rich objects and a limit of 16kb
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// Configure Express app to serve static files from the "public" directory
+app.use(express.static("public"));
+
 export { app };
