2cb18ac8be8d85ff9b66c254fca4466046ad9156
AVidhanR <itsvidhanreddy@gmail.com>
2024-07-03 21:02:44 +0530

    Add like model to backend

---
+++ backend/03_vidtube/src/models/like.models.js
@@ -0,0 +1,29 @@
+/*
+likes [icon: thumbs-up] {
+  id string pk
+  video ObjectId videos
+  comment ObjectId comments
+  tweet ObjectId tweets
+  likedBy ObjectId users
+  createdAt Date
+  updatedAt Date
+}
+*/
+
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+  {
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Like = mongoose.model("Like", likeSchema);
