06e7421d15f034a2631859fd676bfb9b1bb95b5d
AVidhanR <itsvidhanreddy@gmail.com>
2024-07-03 16:00:37 +0530

    Update MongoDB connection to include database name

--- backend/03_vidtube/src/db/index.js
+++ backend/03_vidtube/src/db/index.js
@@ -1,10 +1,17 @@
-import mongoose from "mongoose";
+import mongoose, { connectionInstance } from "mongoose";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+
+    console.log(
+      `\n MongoDB connected: ${connectionInstance.connection.host} \n`
+    );
   } catch (error) {
     console.log("DB error", error.message);
     process.exit(1);
   }
 };
+
+export default connectDB;
