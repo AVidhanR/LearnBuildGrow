c280099789cc9349622fcd2c8cd9bcd100d7d04c
AVidhanR <itsvidhanreddy@gmail.com>
2024-07-03 16:51:33 +0530

    Create healthCheck.route.js

---
+++ backend/03_vidtube/src/routes/healthCheck.route.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { HealthCheck } from "../controllers/healthCheck.controller.js";
+
+const router = Router();
+
+router.get("/", HealthCheck);
+
+/**
+ * or
+ * router.route("/").get(HealthCheck);
+ */
+
+export default router;
