e3630310b488e440251aa72fab025737c2aa7f3a
AVidhanR <itsvidhanreddy@gmail.com>
2024-07-03 20:55:36 +0530

    Add video model to backend

---
+++ backend/03_vidtube/src/models/video.models.js
@@ -0,0 +1,57 @@
+/* 
+videos [icon: video] {
+  id string pk
+  owner ObjectId users
+  videoFile string
+  thumbnail string
+  title string
+  description string
+  duration number
+  views number
+  isPublished boolean
+  createdAt Date
+  updatedAt Date
+}
+*/
+
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("Video", videoSchema);
