ff864a05b2a9abf8f41b82453067efaecf64723e
AVidhanR <itsvidhanreddy@gmail.com>
2024-07-03 21:05:41 +0530

    Add Comment model to backend

---
+++ backend/03_vidtube/src/models/comments.models.js
@@ -0,0 +1,32 @@
+/* 
+comments [icon: comment] {
+  id string pk
+  video ObjectId videos
+  owner ObjectId users
+  content string
+  createdAt Date
+  updatedAt Date
+}
+*/
+
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema(
+  {
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
