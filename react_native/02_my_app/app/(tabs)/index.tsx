4a362d49e72a0bb78b175d564df37b90af7bf4ec
AVidhanR <itsvidhanreddy@gmail.com>
2024-10-13 18:51:14 +0530

    Update index.tsx

--- react_native/02_my_app/app/(tabs)/index.tsx
+++ react_native/02_my_app/app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={Styles.container}>
+      <Text style={Styles.text}>App</Text>
     </View>
   );
 };
 
 export default App;
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
